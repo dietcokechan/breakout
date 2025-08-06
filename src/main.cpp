b2a618ec1dd874dc197463df1a83949b683c0c90
dietcokechan <dietcokechan@gmail.com>
2025-08-06 17:35:03 +0300

    small restructure

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,78 @@
 #include "raylib.h"
 
-int main() {
-  const int screenWidth = 800;
-  const int screenHeight = 600;
-  const int pWidth = 160;
-  const int pHeight = 20;
-  const int bRadius = 16;
+#define screenWidth 800
+#define screenHeight 600
 
-  int pRectX = (screenWidth - pWidth) / 2;
-  int pRectY = (screenHeight - pHeight) / 2;
+// constant variables
+static const int pWidth = 160;
+static const int pHeight = 20;
+static const int bRadius = 16;
 
-  int bPosX = (screenWidth - bRadius) / 2;
-  int bPosY = (screenHeight - bRadius) / 2;
+// global variables
+int pRectX, pRectY;
+int bPosX, bPosY;
+int direction;
+float speed;
 
-  int direction = 1;
-  float speed = 5.0f;
+// functions
+static void InitGame();
+static void UpdateGame();
+static void DrawGame();
+
+// initialization
+void InitGame() {
+  // init player
+  pRectX = (screenWidth - pWidth) / 2;
+  pRectY = (screenHeight - pHeight) / 2;
+
+  // init ball
+  bPosX = (screenWidth - bRadius) / 2;
+  bPosY = (screenHeight - bRadius) / 2;
+
+  // idle ball anim
+  direction = 1;
+  speed = 5.0f;
 
   InitWindow(screenWidth, screenHeight, "Breakout");
   
   SetTargetFPS(60);
+}
+
+// update
+void UpdateGame() {
+  // move ball side to side
+  bPosX += direction * speed;
+  if (bPosX + bRadius >= screenWidth || bPosX - bRadius <= 0) {
+    direction *= -1;
+  }
+
+  // player input
+  if (IsKeyDown(KEY_RIGHT)) pRectX += 5.0f;
+  if (IsKeyDown(KEY_LEFT)) pRectX -= 5.0f;
+}
+
+// drawing
+void DrawGame() {
+  BeginDrawing();
+  ClearBackground(RAYWHITE);
+  
+  // draw player rect
+  DrawRectangle(pRectX, pRectY + 200, pWidth, pHeight, BLACK);
+
+  // draw ball
+  DrawCircle(bPosX, bPosY + 100, bRadius, RED); 
+  
+  DrawFPS(10, 10);
+  
+  EndDrawing();
+}
+
+int main() {
+  InitGame();
 
   while (!WindowShouldClose()) {
-    /************/
-    /** Update **/
-    /************/
-    // move ball side to side
-    bPosX += direction * speed;
-
-    if (bPosX + bRadius >= screenWidth || bPosX - bRadius <= 0) {
-      direction *= -1;
-    }
-    
-    // player input
-    if (IsKeyDown(KEY_RIGHT)) pRectX += 5.0f;
-    if (IsKeyDown(KEY_LEFT)) pRectX -= 5.0f;
-    
-    /**********/
-    /** Draw **/
-    /**********/
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-
-    // player rect
-    DrawRectangle(pRectX, pRectY + 200, pWidth, pHeight, BLACK);
-
-    // ball
-    DrawCircle(bPosX, bPosY + 100, bRadius, RED);
-    
-    EndDrawing();
+    UpdateGame();
+    DrawGame();
   }
   
   CloseWindow();
