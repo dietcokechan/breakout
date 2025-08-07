9daa3ceddc9fe942c616c8cde626026d17235512
dietcokechan <dietcokechan@gmail.com>
2025-08-07 13:33:15 +0300

    fbricks collision and logic implemented | game over loosely implemented | will polish and document next

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include "raylib.h"
+#include <cmath>
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
-#define BRICK_ROWS 6
-#define BRICKS_PER_ROW 12
+#define BRICK_ROWS 5
+#define BRICKS_PER_ROW 10
 
 // structs
 typedef struct Player {
+  Vector2 initialPos;
   Vector2 pos;
   Vector2 size;
 } Player;
 
 typedef struct Ball {
+  Vector2 initialPos;
   Vector2 pos;
   Vector2 speed;
   int radius;
@@ -19,6 +22,7 @@ typedef struct Ball {
 
 typedef struct Brick {
   Vector2 pos;
+  bool active;
 } Brick;
 
 // global variables
@@ -29,6 +33,8 @@ static Brick brick[BRICK_ROWS][BRICKS_PER_ROW];
 static Color colors[3] = {DARKBLUE, BLUE, SKYBLUE};
 static Vector2 brickSize = { 0 };
 static const int brickPadding = 6;
+
+static bool gameOver = false;
 
 // functions
 static void InitGame();
@@ -46,88 +52,143 @@ void CollisionChecks() {
       ball.speed.x = (ball.pos.x - player.pos.x) / (player.size.x / 2) * 5;
     }
   }
+
+  // ball collision with bricks
+  for (auto & i : brick) {
+    for (auto & j : i) {
+      if (j.active) {
+        // hit down
+        if (((ball.pos.y - ball.radius) <= (j.pos.y + brickSize.y/2)) &&
+            ((ball.pos.y - ball.radius) > (j.pos.y + brickSize.y/2 + ball.speed.y)) &&
+            ((std::fabs(ball.pos.x - j.pos.x)) < (brickSize.x/2 + ball.radius * 2/3)) && (ball.speed.y < 0))
+        {
+            j.active = false;
+            ball.speed.y *= -1;
+        }
+        // hit up
+        else if (((ball.pos.y + ball.radius) >= (j.pos.y - brickSize.y/2)) &&
+                ((ball.pos.y + ball.radius) < (j.pos.y - brickSize.y/2 + ball.speed.y)) &&
+                ((std::fabs(ball.pos.x - j.pos.x)) < (brickSize.x/2 + ball.radius * 2/3)) && (ball.speed.y > 0))
+        {
+            j.active = false;
+            ball.speed.y *= -1;
+        }
+        // hit left
+        else if (((ball.pos.x + ball.radius) >= (j.pos.x - brickSize.x/2)) &&
+                ((ball.pos.x + ball.radius) < (j.pos.x - brickSize.x/2 + ball.speed.x)) &&
+                ((std::fabs(ball.pos.y - j.pos.y)) < (brickSize.y/2 + ball.radius * 2/3)) && (ball.speed.x > 0))
+        {
+            j.active = false;
+            ball.speed.x *= -1;
+        }
+        // hit right
+        else if (((ball.pos.x - ball.radius) <= (j.pos.x + brickSize.x/2)) &&
+                ((ball.pos.x - ball.radius) > (j.pos.x + brickSize.x/2 + ball.speed.x)) &&
+                ((std::fabs(ball.pos.y - j.pos.y)) < (brickSize.y/2 + ball.radius * 2/3)) && (ball.speed.x < 0))
+        {
+            j.active = false;
+            ball.speed.x *= -1;
+        }
+      }
+    }
+  }
 }
 
 // initialization
 void InitGame() {
   // init player
   player.size = (Vector2){ SCREEN_WIDTH / 10, 20 };
-  player.pos = (Vector2){ (SCREEN_WIDTH - player.size.x) / 2, SCREEN_HEIGHT * 0.9 };
+  player.pos = player.initialPos = (Vector2){ (SCREEN_WIDTH - player.size.x) / 2, SCREEN_HEIGHT * 0.9 };
 
   // init ball
   ball.radius = 10;
-  ball.pos = (Vector2){ player.pos.x, player.pos.y - player.size.y / 2 - ball.radius };
+  ball.pos = ball.initialPos = (Vector2){ player.pos.x, player.pos.y - player.size.y / 2 - ball.radius };
   ball.speed = (Vector2){ 0, 0 };
 
   // init bricks
-  brickSize = (Vector2){ SCREEN_WIDTH / BRICKS_PER_ROW, 26};
+  brickSize = (Vector2){ SCREEN_WIDTH / BRICKS_PER_ROW, 40};
   int initialPos = 60;
   
   for (int i = 0; i < BRICK_ROWS; i++) {
-    int paddingX = brickSize.x / 2 + brickPadding;
-    
     for (int j = 0; j < BRICKS_PER_ROW; j++) {
-      brick[i][j].pos.x = j * brickSize.x + paddingX;
+      brick[i][j].pos.x = j * brickSize.x + brickSize.x / 2;
       brick[i][j].pos.y = i * brickSize.y + initialPos;
-      
-      paddingX += brickPadding;
+      brick[i][j].active = true;
     }
-    initialPos += brickPadding;
   }
 }
 
 // update
 void UpdateGame() {
-  // ball movement
-  if (ball.pos.x + ball.radius >= SCREEN_WIDTH || ball.pos.x - ball.radius <= 0) {
-    ball.speed.x *= -1;
-  }
-  else if (ball.pos.y + ball.radius >= SCREEN_HEIGHT || ball.pos.y - ball.radius <= 0) {
-    ball.speed.y *= -1;
-  }
+  if (!gameOver) {
+    // ball movement
+    if (ball.pos.x + ball.radius >= SCREEN_WIDTH || ball.pos.x - ball.radius <= 0) {
+      ball.speed.x *= -1;
+    }
+    if (ball.pos.y - ball.radius <= 0) {
+      ball.speed.y *= -1;
+    }
+    if (ball.pos.y + ball.radius >= SCREEN_HEIGHT) {
+      ball.speed = (Vector2){ 0, 0 };
+      gameOver = true;
+    }
 
-  if (IsKeyPressed(KEY_SPACE)) {
-    ball.speed = (Vector2){ 0, -5 };
-  }
-  ball.pos.x += ball.speed.x;
-  ball.pos.y += ball.speed.y;
-  
+    if (IsKeyPressed(KEY_SPACE)) {
+      ball.speed = (Vector2){ 0, -5 };
+    }
+    ball.pos.x += ball.speed.x;
+    ball.pos.y += ball.speed.y;
 
-  // player movement (restrict to window)
-  if (IsKeyDown(KEY_RIGHT)) {
-    player.pos.x += 5.0f;
-  }
-  if (player.pos.x + player.size.x / 2 >= SCREEN_WIDTH) {
-    player.pos.x = SCREEN_WIDTH - (player.size.x / 2);
-  }
+    // player movement (restrict to window)
+    if (IsKeyDown(KEY_RIGHT)) {
+      player.pos.x += 5.0f;
+    }
+    if (player.pos.x + player.size.x / 2 >= SCREEN_WIDTH) {
+      player.pos.x = SCREEN_WIDTH - (player.size.x / 2);
+    }
 
-  if (IsKeyDown(KEY_LEFT)) {
-    player.pos.x -= 5.0f;
-  }
-  if (player.pos.x - player.size.x / 2 <= 0) {
-    player.pos.x = (player.size.x / 2);
-  }
+    if (IsKeyDown(KEY_LEFT)) {
+      player.pos.x -= 5.0f;
+    }
+    if (player.pos.x - player.size.x / 2 <= 0) {
+      player.pos.x = (player.size.x / 2);
+    }
 
-  CollisionChecks();
+    CollisionChecks();
+  }
+  else {
+    if (IsKeyPressed(KEY_SPACE)) {
+      InitGame();
+      gameOver = false;
+    }
+  }
 }
 
 // drawing
 void DrawGame() {
   BeginDrawing();
   ClearBackground(RAYWHITE);
-  
-  // draw player rect
-  DrawRectangle(player.pos.x - player.size.x / 2, player.pos.y - player.size.y / 2, player.size.x, player.size.y, BLACK);
 
-  // draw ball
-  DrawCircleV(ball.pos, ball.radius, MAROON);
+  if (!gameOver) {
+    // draw player rect
+    DrawRectangle(player.pos.x - player.size.x / 2, player.pos.y - player.size.y / 2, player.size.x, player.size.y, BLACK);
 
-  // draw bricks
-  for (int i = 0; i < BRICK_ROWS; i++) {
-    Color brickColor = colors[i % 3];
-    for (int j = 0; j < BRICKS_PER_ROW; j++) {
-      DrawRectangle(brick[i][j].pos.x - brickSize.x / 2, brick[i][j].pos.y - brickSize.y / 2, brickSize.x, brickSize.y, brickColor);
+    // draw ball
+    DrawCircleV(ball.pos, ball.radius, MAROON);
+
+    // draw bricks
+    for (int i = 0; i < BRICK_ROWS; i++) {
+      for (int j = 0; j < BRICKS_PER_ROW; j++) {
+        if (brick[i][j].active) {
+          Color brickColor = colors[(i + j) % 3];
+          DrawRectangle(brick[i][j].pos.x - brickSize.x / 2, brick[i][j].pos.y - brickSize.y / 2, brickSize.x, brickSize.y, brickColor);
+        }
+      }
     }
+  }
+  else {
+    DrawText("PRESS [SPACE] TO PLAY", SCREEN_WIDTH / 2 - MeasureText("PRESS [SPACE] TO PLAY", 20) / 2,
+             SCREEN_HEIGHT / 2 - 50, 20, DARKGRAY);
   }
   
   DrawFPS(10, 10);
